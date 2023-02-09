820521401dd0d24676d3577800ac20254a90b0ba
Onur Eredo <onureredo@yahoo.com>
2023-02-09 16:52:42 +0100

    test

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function About() {
   return (
     <div>
-      <h1>ABOUT COMPONENT</h1>
+      <h1>ABOUT COMPONENTT</h1>
       <Link to="/">Go back to Home here</Link>
       <br />
       <Link to="/pokemon">Go back to PokeFight here</Link>
